895e741ec83b52dac9e87bf94bece82da011915d
Mitt Coats <mittcoats@gmail.com>
2017-05-13 18:36:21 -0400

    update README; update styles and formats to follow python style guide; update comment in Media.py with a docstring and a more detailed description.

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# ud036_StarterCode
-Source code for a Movie Trailer website.
+# Mitt's Movie Trailer Site
+
+This repository displays a webpage of movie posters and plays their trailers
+when you click each movie.
+
+It takes a list of movies, which are described in "entertainment_center.py",
+and uses a constructor in Media.py to build an instance of each movie. This
+list of movie objects in stored in a "movies" variable, which is created in the
+"entertainment_center.py" file. Finally, by running "entertainment_center.py"
+you call the method open_movies_page(), which is located in
+"fresh_tomatoes.py", and takes a list of movies as a parameter.
+
+The file "fresh_tomatoes.py" constructs the webpage. It takes a list of movies,
+the ones previously defined in "entertainment_center.py", and constructs a
+content variable that contains all the information needed by the HTML cards
+for each movie. Finally, it launches builds the HTML file
+"fresh_tomatoes.html", and launches the page in a local browser.
+
+You can run all code by running the script in "entertainment_center.py" file.
+This has a aforementioned open_movies_page() method, which takes care of all
+the work for you. You can do this by
+
+--
+
+Further work would create the list of movies from an external API, instead of
+defining each movie by hand, which would enable a much larger collection of
+movies.
--- media.py
+++ media.py
@@ -1,21 +1,28 @@
 import webbrowser
 
-# Constructor for building movies
+
 class Movie():
-	def __init__(self, 
-							 movie_title,
-							 movie_summary,
-							 movie_poster_img,
-							 movie_trailer_url,
-							 movie_studio,
-							 movie_lead_actor):
+    """
+    This constructor takes an array of values and returns a movie object
+    with each of the values in the array assigned as properties of the object.
 
-		self.title 					= movie_title
-		self.summary 				= movie_summary
-		self.poster_img_url = movie_poster_img
-		self.trailer_url 		= movie_trailer_url
-		self.studio 				= movie_studio
-		self.lead_actor 		= movie_lead_actor
+    The classes show_trailer() method takes a movie object as a parameters, and
+    uses the trailer value to launch a web browser page displaying the trailer.
+    """
+    def __init__(self,
+                 movie_title,
+                 movie_summary,
+                 movie_poster_img,
+                 movie_trailer_url,
+                 movie_studio,
+                 movie_lead_actor):
 
-	def show_trailer(self):
-			webbrowser.open(self.trailer)
+        self.title = movie_title
+        self.summary = movie_summary
+        self.poster_img_url = movie_poster_img
+        self.trailer_url = movie_trailer_url
+        self.studio = movie_studio
+        self.lead_actor = movie_lead_actor
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer)
